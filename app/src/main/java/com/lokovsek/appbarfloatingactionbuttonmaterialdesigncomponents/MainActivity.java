f20b08daf923f064bb384f72a9c5d404398c3330
JureLokovsek <jurext@hotmail.com>
2020-01-04 00:59:28 +0100

    Added butter knife.

--- app/src/main/java/com/lokovsek/appbarfloatingactionbuttonmaterialdesigncomponents/MainActivity.java
+++ app/src/main/java/com/lokovsek/appbarfloatingactionbuttonmaterialdesigncomponents/MainActivity.java
@@ -3,13 +3,21 @@ package com.lokovsek.appbarfloatingactionbuttonmaterialdesigncomponents;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.text) TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        text.setText("Testing");
 
     }
 }
