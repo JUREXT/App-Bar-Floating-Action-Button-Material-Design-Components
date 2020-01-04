75575f9ede58417b67b757c06b23adf13b72624a
JureLokovsek <jurext@hotmail.com>
2020-01-04 01:39:24 +0100

    Created custom app bar.

--- app/src/main/java/com/lokovsek/appbarfloatingactionbuttonmaterialdesigncomponents/MainActivity.java
+++ app/src/main/java/com/lokovsek/appbarfloatingactionbuttonmaterialdesigncomponents/MainActivity.java
@@ -10,14 +10,14 @@ import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.text) TextView text;
+   // @BindView(R.id.text) TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        text.setText("Testing");
+       // text.setText("Testing");
 
     }
 }
